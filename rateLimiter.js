c47a14022d5323a7b642fbea7d469c743bc322dd
udugam <ryan.udugam@gmail.com>
2020-01-15 10:42:27 -0500

    commit before adding ESlint

--- rateLimiter.js
+++ rateLimiter.js
@@ -6,11 +6,15 @@ function rateLimiter(reqLimit, reqTimeLimit) {
     reqHistory.push(timeStamp)
     console.log(reqHistory)
     if(reqHistory.length===1) return true
-    if(reqHistory.length<reqLimit && timeStamp-reqHistory[0]<=reqTimeLimit) { //If number of requests is less than the Limit and less than the time Limit
+    if(reqHistory.length<=reqLimit && timeStamp-reqHistory[0]<=reqTimeLimit) { //If number of requests is less than the Limit and less than the time Limit
         return true
     } else {
         return false
     }    
 }
 
-module.exports = rateLimiter
+function clearReqHistory() {
+    reqHistory = []
+}
+
+module.exports = {rateLimiter, clearReqHistory}
--- tests/rateLimiter.test.js
+++ tests/rateLimiter.test.js
@@ -1,13 +1,33 @@
-const rateLimiter = require("../rateLimiter.js")
+const rateLimiter = require("../rateLimiter.js").rateLimiter
+const clearReqHistory = require("../rateLimiter.js").clearReqHistory
 const reqLimit = 4
-const reqTimeLimit = 5000
+const reqTimeLimit = 1000
+let result = ""
+
+//Set Default jest timeout to 1 minute
+jest.setTimeout(60000)
 
 function timeout(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-test('Mimics 2 requests in 4 seconds', async () => {
-    rateLimiter(reqLimit,reqTimeLimit)
-    await timeout(4000)
-    expect(rateLimiter(reqLimit,reqTimeLimit)).toBe(true);
+test('Tests 5 requests in 1 second', async () => {
+    for(let i=0; i<5; i++) {
+        result = rateLimiter(reqLimit,reqTimeLimit)
+        if(result===false) break
+        await timeout(200)
+    }
+    expect(result).toBe(false);
 });
+
+test('Tests 4 requests in 5 seconds', async () => {
+    clearReqHistory()
+    for(let i=0; i<4; i++) {
+        result = rateLimiter(reqLimit,reqTimeLimit)
+        if(result===false) break
+        await timeout(250)
+    }
+    expect(result).toBe(true);
+});
+
+
