71a8bfd085e72c4a8b7693551600d8f0e8eb74bc
udugam <ryan.udugam@gmail.com>
2020-01-16 16:12:15 -0500

    added simple navigation and page rendering using material UI components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Import Material UI Components
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  AppBar,
+  Toolbar,
+  Typography,
+  withStyles,
+  Button
+} from '@material-ui/core';
+
+//Import Custom Components
+import Events from './components/Events'
+import Poi from './components/Poi'
+import Stats from './components/Stats'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+class App extends Component {
+
+  state = {
+    currentPage: "home"
+  }
+  
+  handlePageChange = (pageName) => {
+    this.setState({currentPage: pageName})
+  }
+  
+  render() {
+    const {classes} = this.props
+    return(
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Welcome to EQ Works 😎
+            </Typography>
+            <Button 
+              color="inherit" 
+              variant = {this.state.currentPage === "events" ? "outlined" : "text"} 
+              onClick = {() => this.handlePageChange("events")}
+            > 
+              Events
+            </Button>
+            <Button 
+              color="inherit"
+              variant = {this.state.currentPage === "stats" ? "outlined" : "text"} 
+              onClick = {() => this.handlePageChange("stats")}
+            >
+              Stats
+            </Button>
+            <Button 
+              color="inherit"
+              variant = {this.state.currentPage === "poi" ? "outlined" : "text"} 
+              onClick = {() => this.handlePageChange("poi")}
+            >
+              Points of Interest
+            </Button>
+          </Toolbar>
+        </AppBar>
+        {this.state.currentPage === "events" &&
+          <Events />
+        }
+        {this.state.currentPage === "poi" &&
+          <Poi />
+        }
+        {this.state.currentPage === "stats" &&
+          <Stats />
+        }
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withStyles(useStyles)(App);
---
+++ client/src/components/Poi.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Poi extends Component {
+    render() {
+        return(
+            <div>
+                Poi
+            </div>
+        )
+    }
+}
+
+export default Poi
