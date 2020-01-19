667681aaa22920e6b75d5ac28b79cb857e14c9f3
udugam <ryan.udugam@gmail.com>
2020-01-18 23:34:02 -0500

    added hourly stats to chart

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -4,8 +4,11 @@ import {
   Grid,
   FormGroup,
   FormControlLabel,
-  Checkbox
+  Checkbox,
+  Select,
+  MenuItem
 } from "@material-ui/core"
+import moment from 'moment'
 
 class Stats extends Component {
     state = {
@@ -19,10 +22,14 @@ class Stats extends Component {
         }
     }
 
-    handleChange = (metric, metricState) => {
+    handleMetricChange = (metric, metricState) => {
       let metrics = this.state.metrics
       metrics[metric] = !metricState
       this.setState({metrics},this.massageData)
+    }
+
+    handleTimeChange = (event) => {
+      this.setState({timeFrame : event.target.value}, this.fetchData)
     }
 
     fetchData = () => {
@@ -37,17 +44,33 @@ class Stats extends Component {
         let headings = ['Day']
         let metrics = this.state.metrics
         for(var key in metrics) {
-          if(metrics[key] === true) headings.push(key) 
+          if(metrics[key] === true) {
+            if(key === 'impressions') headings.push('impressions X 1000')
+            else 
+              headings.push(key) 
+          }
         }
         massagedData.push(headings)
   
         //  Loop through rawData and push data in corrected format into massagedData Array
         let rawData = this.state.rawData
         rawData.forEach(stat => {
-          let row = [new Date(stat.date)]
+          let row = []
+          if(this.state.timeFrame === 'hourly') {
+            row.push(moment(stat.date).add(stat.hour,'h').toDate())
+          } else {
+            row.push(new Date(stat.date))
+          }
+
           let metrics = this.state.metrics
           for(var key in metrics) {
-            if(metrics[key] === true) row.push(Number(stat[key])) 
+            if(metrics[key] === true) {
+              if(key === 'impressions') {
+                row.push(Number(stat[key])/1000) 
+              } else {
+                row.push(Number(stat[key])) 
+              }
+            }
           }
           massagedData.push(row)
         })
@@ -72,22 +95,29 @@ class Stats extends Component {
             >
               <Grid item>
                 <FormGroup row>
+                  <Select
+                    value={this.state.timeFrame}
+                    onChange={this.handleTimeChange}
+                  >
+                    <MenuItem value='daily'>Daily</MenuItem>
+                    <MenuItem value='hourly'>Hourly</MenuItem>
+                  </Select>
                   <FormControlLabel
                     control={
-                      <Checkbox checked={this.state.metrics.impressions} onChange={()=>this.handleChange('impressions',this.state.metrics.impressions)} value="impressions"/>
+                      <Checkbox checked={this.state.metrics.impressions} onChange={()=>this.handleMetricChange('impressions',this.state.metrics.impressions)} value="impressions"/>
                       // <Checkbox    />
                     }
                     label="Impressions"
                   />
                   <FormControlLabel
                     control={
-                      <Checkbox checked={this.state.metrics.clicks} onChange={()=>this.handleChange('clicks',this.state.metrics.clicks)} value="clicks"/>
+                      <Checkbox checked={this.state.metrics.clicks} onChange={()=>this.handleMetricChange('clicks',this.state.metrics.clicks)} value="clicks"/>
                     }
                     label="Clicks"
                   />
                   <FormControlLabel
                     control={
-                      <Checkbox checked={this.state.metrics.revenue} onChange={()=>this.handleChange('revenue',this.state.metrics.revenue)} value="revenue"/>
+                      <Checkbox checked={this.state.metrics.revenue} onChange={()=>this.handleMetricChange('revenue',this.state.metrics.revenue)} value="revenue"/>
                     }
                     label="Revenue"
                   />
@@ -95,14 +125,13 @@ class Stats extends Component {
               </Grid>
               <Grid item >
                 <Chart
-                    width={'600px'}
-                    height={'400px'}
+                    width={'75vw'}
+                    height={'75vh'}
                     chartType="Line"
                     loader={<div>Loading Chart</div>}
                     data={this.state.massagedData}
                     options={{
                         chart: {
-                        title: 'Daily Events',
                         legend: 'none'
                         },
                     }}
